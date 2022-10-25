1b9b96e1e84d8d2e7c3971df27aeb0581f2d4431
Thiago Freitas <thiagofreitas2909@gmail.com>
2022-10-25 00:39:58 -0300

    AAA

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,10 @@ import { Theme } from './styles/themes/default'
 
 export function App() {
   return (
-<<<<<<< HEAD
-    <Router>
-      <Header />
-      <Container>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/anime-list" element={<AnimeList />} />
-          <Route path="/manga-list" element={<MangaList />} />
-          <Route path="/Waifus" element={<Waifus />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/browse" element={<Browse />} />
-          <Route path="/forum" element={<Forum />} />
-        </Routes>
-      </Container>
-      <Footer />
-    </Router>
-=======
     <ThemeProvider theme={Theme}>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
     </ThemeProvider>
->>>>>>> beta
   )
 }
