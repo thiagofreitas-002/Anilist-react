f60b2a42a002c8ee2dfb483235e1b1eec13045d6
Thiago Freitas <thiagofreitas2909@gmail.com>
2022-10-25 00:44:18 -0300

    Pequenas alterações

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -53,7 +53,7 @@ export function Header() {
               </li>
 
               <li>
-                <Link to="/images">Images</Link>
+                <Link to="/waifus">Waifus</Link>
               </li>
 
               <li>
@@ -74,7 +74,7 @@ export function Header() {
             <A.User>
               <a href=""></a>
               <img
-                src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cb6d15ee-9f60-434a-9a5d-d91026e33e0a/d7til5w-2f3260a3-7092-47b4-aad3-d921b361cc4b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2NiNmQxNWVlLTlmNjAtNDM0YS05YTVkLWQ5MTAyNmUzM2UwYVwvZDd0aWw1dy0yZjMyNjBhMy03MDkyLTQ3YjQtYWFkMy1kOTIxYjM2MWNjNGIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pD52YTUbCEL4DLf6lnWcLUJFI9gpFglFsfO5xLAMErQ"
+                src="https://i.pinimg.com/736x/e7/fe/70/e7fe701cff4a18cdd05ae67b79ef8376.jpg"
                 alt="profile__img"
               />
             </A.User>
